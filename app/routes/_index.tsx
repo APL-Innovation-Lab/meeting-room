927777914d70c07cf613d7a7ae3cf4e6fc78a6f2
NBornstein278 <nategbornstein@gmail.com>
2024-08-01 17:01:50 -0500

    Add Header and Spacing to Header Image

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,22 +1,31 @@
-import { Card, CardGroup, CardHeader } from "@trussworks/react-uswds";
+import { Card, CardGroup, CardHeader, Header } from "@trussworks/react-uswds";
 import { mergeMeta } from "~/lib/merge-meta";
 
 export const meta = mergeMeta(({ parentTitle }) => [{ title: `Meeting Spaces • ${parentTitle}` }]);
 
 export default function Index() {
     return (
-        <CardGroup>
-            <Card>
-                <CardHeader>
-                    <h1 className="usa-card__heading">Meeting Spaces</h1>
-                    <p className="font-sans text-base-darker">
-                        Austin Public Library Meeting Spaces are
-                        <strong className="font-bold"> free of charge</strong> and ideal for
-                        discussion groups, panels, and lectures. Both paper and online reservation
-                        requests are timestamped and processed in the order they are received.
-                    </p>
-                </CardHeader>
-            </Card>
-        </CardGroup>
+        <>
+            <Header>
+                <img
+                    src="public\meetingSpacesHeader.jpg"
+                    className="max-w-5xl max-h-52 justify-center"
+                />
+            </Header>
+            <CardGroup>
+                <Card>
+                    <CardHeader>
+                        <h1 className="usa-card__heading">Meeting Spaces</h1>
+                        <p className="font-sans text-base-darker">
+                            Austin Public Library Meeting Spaces are
+                            <strong className="font-bold"> free of charge</strong> and ideal for
+                            discussion groups, panels, and lectures. Both paper and online
+                            reservation requests are timestamped and processed in the order they are
+                            received.
+                        </p>
+                    </CardHeader>
+                </Card>
+            </CardGroup>
+        </>
     );
 }
