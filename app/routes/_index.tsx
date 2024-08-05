c79370e06c1ed392e28b322022a2c980464b513c
NBornstein278 <nategbornstein@gmail.com>
2024-08-05 13:19:31 -0500

    Group All Components in Single Flex Div & Add Select/Label Components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,10 @@ import {
     CardGroup,
     CardHeader,
     Header,
+    Label,
+    Select,
 } from "@trussworks/react-uswds";
+import React from "react";
 import { mergeMeta } from "~/lib/merge-meta";
 
 export const meta = mergeMeta(({ parentTitle }) => [{ title: `Meeting Spaces • ${parentTitle}` }]);
@@ -14,42 +17,60 @@ export const meta = mergeMeta(({ parentTitle }) => [{ title: `Meeting Spaces •
 export default function Index() {
     return (
         <>
-            <Header>
-                <div className="flex justify-center">
-                    <img
-                        alt="Public Meeting Room"
-                        className="min-w-[40rem] max-w-[62rem] object-contain"
-                        loading="lazy"
-                        src="/meetingSpacesHeader.jpg"
-                    />
+            <div className="flex justify-center">
+                <div className="min-w-[30rem] max-w-[62rem]">
+                    <Header>
+                        <div className="min-w-[30rem] max-w-[62rem] object-contain">
+                            <img
+                                alt="Public Meeting Room"
+                                loading="lazy"
+                                src="/meetingSpacesHeader.jpg"
+                            />
+                        </div>
+                        <BreadcrumbBar>
+                            <Breadcrumb>
+                                <BreadcrumbLink href="#">
+                                    <span>Home</span>
+                                </BreadcrumbLink>
+                            </Breadcrumb>
+                            <Breadcrumb>
+                                <BreadcrumbLink href="#">
+                                    <span>Meeting Spaces</span>
+                                </BreadcrumbLink>
+                            </Breadcrumb>
+                        </BreadcrumbBar>
+                    </Header>
+                    <CardGroup>
+                        <Card>
+                            <CardHeader>
+                                <h1 className="font usa-card__heading font-bold py-2">
+                                    Meeting Spaces
+                                </h1>
+                                <p className="font-sans text-base-darker text-left">
+                                    Austin Public Library Meeting Spaces are
+                                    <strong className="font-bold"> free of charge</strong> and ideal
+                                    for discussion groups, panels, and lectures. Both paper and
+                                    online reservation requests are timestamped and processed in the
+                                    order they are received.
+                                </p>
+                            </CardHeader>
+                            <div className="flex-col px-3">
+                                <Label htmlFor="reservation-select">Reserve Online</Label>
+                                <Select id="reservation-select" name="reservation-select">
+                                    <React.Fragment key=".0">
+                                        <option disabled>- Select - </option>
+                                        <option value="non-profit">
+                                            Room for Non-Profit/Non-Commercial Activity
+                                        </option>
+                                        <option value="business">
+                                            Room for Business/Company Work
+                                        </option>
+                                    </React.Fragment>
+                                </Select>
+                            </div>
+                        </Card>
+                    </CardGroup>
                 </div>
-            </Header>
-            <div className="flex justify-center">
-                <BreadcrumbBar className="min-w-[40rem]">
-                    <Breadcrumb>
-                        <BreadcrumbLink href="#">
-                            <span>Home</span>
-                        </BreadcrumbLink>
-                    </Breadcrumb>
-                </BreadcrumbBar>
-            </div>
-            <div className="flex justify-center">
-                <CardGroup>
-                    <Card className="min-w-[41rem] max-w-[63rem]">
-                        <CardHeader>
-                            <h1 className="font usa-card__heading font-bold py-2">
-                                Meeting Spaces
-                            </h1>
-                            <p className="font-sans text-base-darker text-left mx-4">
-                                Austin Public Library Meeting Spaces are
-                                <strong className="font-bold"> free of charge</strong> and ideal for
-                                discussion groups, panels, and lectures. Both paper and online
-                                reservation requests are timestamped and processed in the order they
-                                are received.
-                            </p>
-                        </CardHeader>
-                    </Card>
-                </CardGroup>
             </div>
         </>
     );
