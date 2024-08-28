086a5e7174aabcef75a0eb70e414d945ebd9416b
NBornstein278 <nategbornstein@gmail.com>
2024-08-28 13:07:15 -0500

    Enforce no-underline for Breadcrumb Link Text

--- app/components/Breadcrumbs.tsx
+++ app/components/Breadcrumbs.tsx
@@ -17,7 +17,7 @@ const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ links }) => {
                 {links.map((item, index) => (
                     <Breadcrumb className="relative left-0" key={index}>
                         <BreadcrumbLink href={item.href}>
-                            <span>{item.text}</span>
+                            <span className="!no-underline">{item.text}</span>
                         </BreadcrumbLink>
                     </Breadcrumb>
                 ))}
