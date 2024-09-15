4de2cbafe59ee80f1810258835b596895840fa11
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-09-15 16:26:03 -0500

    Add BranchCard Component

--- app/components/BranchCard.tsx
+++ app/components/BranchCard.tsx
@@ -1,4 +1,4 @@
-import { CardGroup } from "@trussworks/react-uswds";
+import { Card } from "@trussworks/react-uswds";
 import React from "react";
 
 interface BranchCardItem {
@@ -13,10 +13,28 @@ interface BranchCardProps {
     links: BranchCardItem[];
 }
 
-const BranchCards: React.FC<BranchCardProps> = ({}) => {
+export const BranchCard: React.FC<BranchCardProps> = ({ ...BranchCardProps }) => {
     return (
         <>
-            <CardGroup></CardGroup>
+            <Card data-id="branch-card">
+                <div className="flex">
+                    <img
+                        alt="An Austin Public Library Branch"
+                        className=""
+                        loading="lazy"
+                        src="/central-library.png"
+                    />
+                    <div>
+                        <div className="text-primary-default text-sans-lg font-bold px-3 mb-1">
+                            Carver Branch
+                        </div>
+                        <p className="px-3 text-sans-md">710 W Cesar Chavez</p>
+                        <div className="px-3 my-4">
+                            <strong className="text-sans-md">2 Available</strong>
+                        </div>
+                    </div>
+                </div>
+            </Card>
         </>
     );
 };
