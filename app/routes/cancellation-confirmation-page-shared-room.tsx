68c7fa6e4f947ea8fd19dfb21f00d977922c2e38
Ashwin Prakash <60802462+apwlhya@users.noreply.github.com>
2025-12-21 21:25:09 -0800

    Edit cancel page spacing to match designs

--- app/routes/cancellation-confirmation-page-shared-room.tsx
+++ app/routes/cancellation-confirmation-page-shared-room.tsx
@@ -11,51 +11,35 @@ export default function CancellationConfirmationPageSharedRoom() {
             <CardGroup className="min-w-[50rem] max-w-[49rem]">
                 <Card>
                     <div className="justify-center ml-5 mr-5">
-                        <h1 className="font-sans text-[40px] font-bold text-center">
+                        <h1 className="font-sans text-[40px] font-bold text-center m-0 pt-4">
                             Canceled
                         </h1>
-                        <h3 className="font-sans text-sans-xs text-center">
+                        <h3 className="font-sans text-sans-xs text-center pt-3">
                             The following has been canceled.
                         </h3>
                         <div className="flex-col px-3">
-                            <div className="my-5">
+                            <div className="flex-col pb-[20px]">
                                 <h3 className="font-sans text-[22px] font-bold text-center">
                                     Austin Central Library, Shared Learning #3
                                 </h3>
                                 <p className="font-sans text-sans-xs text-center">
                                     710 W Cesar Chavez St, Austin, TX 78702
                                 </p>
-                                <br />
-                                <p className="font-sans text-sans-xs text-center">Mon 3/4/24</p>
-                                <p className="font-sans text-sans-xs text-center">
-                                    9:00 AM to 9:15 AM
-                                </p>
-                                <p className="font-sans text-sans-xs text-center">Capacity: 4</p>
-                                <br />
-                                <br />
-                                <div className="flex justify-center">
-                                    <Link href="/">
-                                        <Button
-                                            className="font-sans text-sans-xs"
-                                            style={{
-                                                background: "#1E6F98",
-                                                paddingTop: 15,
-                                                paddingBottom: 15,
-                                                paddingLeft: 15,
-                                                paddingRight: 15,
-                                                color: "#FFFFFFFF",
-                                            }}
-                                            type="button"
-                                        >
-                                            Back to Meeting Spaces
-                                        </Button>
-                                    </Link>
-                                </div>
-                                <br />
-                                <br />
-                                <br />
-                                <br />
-                                <br />
+                            </div>
+                            <p className="font-sans text-sans-xs text-center">Mon 3/4/24</p>
+                            <p className="font-sans text-sans-xs text-center">
+                                9:00 AM to 9:15 AM
+                            </p>
+                            <p className="font-sans text-sans-xs text-center">Capacity: 4</p>
+                            <div className="flex justify-center pt-7 pb-[194px]">
+                                <Link href="/">
+                                    <Button
+                                        className="w-[228px] font-sans text-sans-xs bg-[#016E98] text-white pointer-events-none mr-0"
+                                        type="button"
+                                    >
+                                        Back to Meeting Spaces
+                                    </Button>
+                                </Link>
                             </div>
                         </div>
                     </div>
--- app/routes/cancellation-page-meeting-room.tsx
+++ app/routes/cancellation-page-meeting-room.tsx
@@ -1,7 +1,6 @@
-import { Button, Card, CardGroup, CardHeader, Link } from "@trussworks/react-uswds";
+import { Button, Card, CardGroup, Link } from "@trussworks/react-uswds";
 
 import { mergeMeta } from "~/lib/merge-meta";
-import { useNavigate } from "react-router";
 
 export const meta = mergeMeta(({ parentTitle }) => [{ title: `Cancellation page • Meeting Room` }]);
 
@@ -10,72 +9,43 @@ export default function CancellationPageMeetingRoom() {
         <div className="flex justify-center">
             <CardGroup className="min-w-[50rem] max-w-[49rem]">
                 <Card>
-
                     <div className="justify-center ml-5 mr-5">
-                        <CardHeader className="-mt-3">
-
-                        </CardHeader>
-                        <br />
-                        <br />
-                        <h3 className="font-sans text-sans-xs text-center">
+                        <h3 className="font-sans text-sans-xs text-center pt-4">
                             Are you sure you want to cancel for the room below?
                         </h3>
-
-                        <div className="flex-col px-3">
-                            <div className="my-5">
+                        <div className="flex-col pt-3 px-3">
+                            <div className="flex-col pb-[20px]">
                                 <h3 className="font-sans text-[22px] font-bold text-center">
                                     Carver Branch, #1
                                 </h3>
                                 <p className="font-sans text-sans-xs text-center">
                                     710 W Cesar Chavez St, Austin, TX 78702
                                 </p>
-                                <br />
-                                <p className="font-sans text-sans-xs text-center">Mon 3/4/24</p>
-                                <p className="font-sans text-sans-xs text-center">
-                                    9:00 AM to 9:15 AM
-                                </p>
-                                <p className="font-sans text-sans-xs text-center">Capacity: 4</p>
-                                <br />
-
-                                <div className="flex justify-center">
-                                    <Link href="cancellation-confirmation-page-meeting-room">
-                                        <Button
-                                            className="font-sans text-sans-xs"
-                                            style={{
-                                                background: "#1E6F98",
-                                                paddingTop: 15,
-                                                paddingBottom: 15,
-                                                paddingLeft: 15,
-                                                paddingRight: 15,
-                                                color: "#FFFFFFFF",
-                                            }}
-                                            type="button"
-                                        >
-                                            Yes, Cancel Request
-                                        </Button>
-                                    </Link>
-                                </div>
-                                <br />
-                                <div className="flex justify-center">
-                                    <Link href="/">
-                                        <Button
-                                            className="font-sans text-sans-xs usa-button usa-button--outline"
-                                            style={{
-                                                paddingTop: 15,
-                                                paddingBottom: 15,
-                                                paddingLeft: 35,
-                                                paddingRight: 35,
-                                            }}
-                                            type="button"
-                                        >
-                                            No, Back to Meeting Spaces
-                                        </Button>
-                                    </Link>
-                                </div>
-                                <br />
-                                <br />
-                                <br />
-                                <br />
+                            </div>
+                            <p className="font-sans text-sans-xs text-center">Mon 3/4/24</p>
+                            <p className="font-sans text-sans-xs text-center">
+                                9:00 AM to 9:15 AM
+                            </p>
+                            <p className="font-sans text-sans-xs text-center">Capacity: 4</p>
+                            <div className="flex justify-center pt-7">
+                                <Link href="cancellation-confirmation-page-meeting-room">
+                                    <Button
+                                        className="w-[206px] font-sans text-sans-xs bg-[#016E98] text-white pointer-events-none mr-0"
+                                        type="button"
+                                    >
+                                        Yes, Cancel Request
+                                    </Button>
+                                </Link>
+                            </div>
+                            <div className="flex justify-center pt-[18px] pb-[208px]">
+                                <Link href="/">
+                                    <Button
+                                        className="usa-button--outline w-[232px] font-sans text-sans-xs bg-transparent text-[#026E98] border-[#026E98] pointer-events-none mr-0"
+                                        type="button"
+                                    >
+                                        No, Back to Meeting Spaces
+                                    </Button>
+                                </Link>
                             </div>
                         </div>
                     </div>
