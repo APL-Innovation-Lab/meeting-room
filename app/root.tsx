2953151e8c83b5afefd229d40cfae890e484b612
Mark Malstrom <mark@malstrom.me>
2026-02-03 14:45:33 -0600

    Remove meta and links exports and components

--- app/root.tsx
+++ app/root.tsx
@@ -1,42 +1,34 @@
-import type { LinksFunction } from "react-router";
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
-import { site } from "~/lib/site";
-
 import uswdsReact from "@trussworks/react-uswds/lib/index.css?url";
+import { Outlet, Scripts, ScrollRestoration, useRouteLoaderData } from "react-router";
+import { site } from "~/lib/site";
 import tailwind from "~/styles/tailwind.css?url";
 import { Route } from "./+types/root";
 
 export async function loader({ request }: Route.LoaderArgs) {
-    return { url: request.url };
+    return request.url;
 }
 
-export const meta: Route.MetaFunction = ({ data }) => [
-    { charSet: "utf-8" },
-    { title: site.title },
-    { name: "viewport", content: "width=device-width, initial-scale=1" },
-    { name: "description", content: site.description },
-    { name: "og:title", content: site.title },
-    { name: "og:type", content: "website" },
-    { name: "og:image", content: site.socialMediaImage.light },
-    { name: "og:description", content: site.description },
-    { name: "og:url", content: data.url },
-];
-
-export const links: LinksFunction = () => [
-    { rel: "icon", type: "image/svg+xml", href: site.favicon },
-    { rel: "apple-touch-icon", href: site.homeScreenIcon },
-    { rel: "stylesheet", href: tailwind },
-    { rel: "stylesheet", href: uswdsReact },
-];
-
 export function Layout({ children }: { children: React.ReactNode }) {
+    const url = useRouteLoaderData<Route.ComponentProps["loaderData"]>("root");
+
     return (
         <html lang="en">
             <head>
                 <meta charSet="utf-8" />
                 <meta content="width=device-width, initial-scale=1" name="viewport" />
-                <Meta />
-                <Links />
+
+                <title>{site.title}</title>
+                <meta name="og:title" content={site.title} />
+                <meta name="description" content={site.description} />
+                <meta name="og:description" content={site.description} />
+                <meta name="og:type" content="website" />
+                <meta name="og:image" content={site.socialMediaImage.light} />
+                {url ? <meta name="og:url" content={url} /> : null}
+
+                <link rel="icon" type="image/svg+xml" href={site.favicon} />
+                <link rel="apple-touch-icon" href={site.homeScreenIcon} />
+                <link rel="stylesheet" href={tailwind} />
+                <link rel="stylesheet" href={uswdsReact} />
             </head>
             <body className="bg-base-lightest py-4 px-15">
                 {children}
