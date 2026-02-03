aecc2f0e0d89bbfd38c79295b71c3b4c3c6e103e
Mark Malstrom <mark@malstrom.me>
2026-02-02 23:56:15 -0600

    Update React Router config

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,7 +2,8 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
     future: {
-        unstable_viteEnvironmentApi: true,
-        unstable_splitRouteModules: true,
+        v8_splitRouteModules: true,
+        v8_viteEnvironmentApi: true,
+        v8_middleware: true,
     },
 } satisfies Config;
