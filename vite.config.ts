2e5757740b81219587d3b346cef44d445093f409
Mark Malstrom <mark@malstrom.me>
2026-02-03 01:43:56 -0600

    Add dev tools json Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 // import { ValidateEnv as env } from "@julr/vite-plugin-validate-env";
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [reactRouter(), tsconfigPaths()],
+    plugins: [reactRouter(), tsconfigPaths(), devtoolsJson()],
 });
